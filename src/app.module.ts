710b842b363930d19d0571b694fb2b254df31eea
tong_anh <anhtt@vuihoc.vn>
2022-09-21 10:01:25 +0700

    example module

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './src/modules/users/users.module';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [UsersModule, CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
